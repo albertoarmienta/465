b337af9cb2d38896f374e43cd8aaa91c56748cf4
albertoarmienta <albertoarmienta@gmail.com>
2015-10-25 15:37:41 -0700

    working version of wbib

--- wbib/app/models/reference.rb
+++ wbib/app/models/reference.rb
@@ -1,5 +1,6 @@
 class Reference < ActiveRecord::Base
   validates :url, presence: true
-  validates :url, format: { with: URI.regexp }
+  #validates :url, format: { with: URI.regexp }
+  validates :url, format: { with: /((https|http):\/\/)?(www\.)?(\w+\.)+([a-z]*)(.*)/ }
   belongs_to :topic
 end
--- wbib/db/seeds.rb
+++ wbib/db/seeds.rb
@@ -9,6 +9,24 @@
 topics = Topic.create(
   [
     {title: 'Cool Stuff', description: ' I hear there is alot of cool stuff in here!'},
-    {title: 'Shitty stuff', description: ' Probabaly shouldn\'t go here, cause I hear all the stuff smells like shit'} 
+    {title: 'Shitty stuff', description: ' Probabaly shouldn\'t go here, cause I hear all the stuff smells like shit'},
+    {title: 'syzurp', description: ' yep no tit ass'},
+    {title: 'flame stuff', description: ' flames and other stuff'}
+  ]
+)
+references = Reference.create(
+  [
+    {url: 'google.com', topic_id: topics[0].id},
+    {url: 'reddit.com', topic_id: topics[0].id},
+    {url: 'msn.com', topic_id: topics[0].id},
+    {url: 'poop.com', topic_id: topics[0].id},
+    {url: 'google.com', topic_id: topics[1].id},
+    {url: 'reddit.com', topic_id: topics[1].id},
+    {url: 'msn.com', topic_id: topics[1].id},
+    {url: 'poop.com', topic_id: topics[1].id},
+    {url: 'google.com', topic_id: topics[2].id},
+    {url: 'reddit.com', topic_id: topics[2].id},
+    {url: 'msn.com', topic_id: topics[2].id},
+    {url: 'poop.com', topic_id: topics[2].id},
   ]
 )
